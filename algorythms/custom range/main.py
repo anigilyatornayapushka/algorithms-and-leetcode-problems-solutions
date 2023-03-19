90636af01fce06b12c42f670f224d7ad88a8f487
anigilyatornayapushka <zinchenkobogdan123@gmail.com>
2023-03-19 23:34:00 +0600

    change custom range

--- algorythms/custom range/main.py
+++ algorythms/custom range/main.py
@@ -1,35 +1,19 @@
-def custom_range(start: int, end: int = None, step: int = None, /) -> list[int]:
-    data: list = []
-    if end is None and step is None:
-        if not isinstance(start, int):
-            raise TypeError
+class Range:
+    """Custom range."""
+    def __init__(
+            self,
+            start: int,
+            stop: int = 0,
+            step: int = 1,
+            /) -> None:
+        self.start = start
+        self.stop = stop
+        if stop is 0:
+            self.start, self.stop = self.stop, self.start
+        self.step = step
 
-        val = 0
-        while val < start:
-            data.append(val)
-            val += 1
-        del val
 
-    elif step is None:
-        if not isinstance(start, int) or not isinstance(end, int):
-            raise TypeError
-
-        val = start
-        while val < end:
-            data.append(val)
-            val += 1
-        del val
-
-    else:
-        if not isinstance(start, int) or not isinstance(end, int) or not isinstance(step, int):
-            raise TypeError
-
-        val = start
-        if step == 0 or step < 0 and start < end or step > 0 and start > end:
-            return []
-        while start <= val < end:
-            data.append(val)
-            val += step
-        del val
-
-    return data
+    def __iter__(self) -> int:
+        while self.start < self.stop:
+            yield self.start
+            self.start += self.step
