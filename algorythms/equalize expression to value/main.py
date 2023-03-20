682f15109e0d829da8dc4e25e48359cd5717e98b
anigilyatornayapushka <zinchenkobogdan123@gmail.com>
2023-03-20 16:18:03 +0600

    add new algorythm

---
+++ algorythms/equalize expression to value/main.py
@@ -0,0 +1,38 @@
+answers = set()
+answers_count = 0
+
+def calc(ex, sol, ex_length, n, oper):
+    global answers, answers_count
+
+    if len(answers) >= answers_count:
+        return
+    try:
+        if eval(ex) == sol:
+            answers.add(ex)
+    except ZeroDivisionError:
+        return
+    
+    for i in range(n, ex_length):
+        new_ex = ex[:i] + oper + ex[i:]
+        calc(new_ex, sol, len(new_ex), i+2, "+")
+        calc(new_ex, sol, len(new_ex), i+2, "-")
+        calc(new_ex, sol, len(new_ex), i+2, "*")
+        calc(new_ex, sol, len(new_ex), i+2, "/")
+
+
+def main():
+    global answers_count
+
+    ex = input('expression : ')
+    sol = int(input('equalize to : '))
+    answers_count = int(input('minimum count of answers : '))
+
+    calc(ex, sol, len(ex), 1, "+")
+    calc(ex, sol, len(ex), 1, "-")
+    calc(ex, sol, len(ex), 1, "*")
+    calc(ex, sol, len(ex), 1, "/")
+
+    return list(answers)
+
+
+print(main())
