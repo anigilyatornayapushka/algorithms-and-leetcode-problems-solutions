0e302a8b656f0dbc056fae46d0b79251ae73535a
anigilyatornayapushka <zinchenkobogdan123@gmail.com>
2023-08-03 21:02:40 +0600

    merge-k-sorted-lists

---
+++ problems/merge-k-sorted-lists/main.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/merge-k-sorted-lists/submissions/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Solution №1
+
+class Solution:
+    def mergeKLists(self, lists):
+        res = []
+        
+        for l in lists:
+            while l:
+                res.append(l.val)
+                l = l.next
+
+        def fill(nums):
+            if not nums:
+                return None
+            nodelist = ListNode(nums[0])
+            nodelist.next = fill(nums[1:])
+            return nodelist
+
+        ret = fill(sorted(res))
+        
+        return ret
+
+# Solution №2
+
+class Solution:
+    def mergeKLists(self, lists):
+        res = []
+        
+        for l in lists:
+            while l:
+                res.append(l.val)
+                l = l.next
+
+        vals = sorted(res, reverse=True)
+        nodelist = None
+        for n in vals:
+            nodelist = ListNode(n, nodelist)
+
+        return nodelist
